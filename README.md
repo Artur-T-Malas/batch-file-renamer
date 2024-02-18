4d283a34ba15e3ba56c5cbf00e841ad90680d15a
Artur Małas <artur.malas25@gmail.com>
2024-02-18 18:36:23 +0100

    Fix renaming not working

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 ### WIP
 
 ### Functional Requirements
-User Story: As a User, I want to input the amount and choose my currency
-1. Get amount using input text field and choose currency from a dropdown list
+User Story: As a User, I want to choose files from a dialog window
+1. Get paths to files and their current names from a dialog window
    - Use PyQt GUI elements for desktop app
 
-User Story: As a User, I want to see the result of the conversion to my chosen currency
-1. Get the output currency from a dropdown list
+User Story: As a User, I want to set the new name for the file batch
+1. Get the desired name from an input field from the user
    - Use PyQt
-2. Get the conversion rate for the chosen currency from a key-less API
+2. Change the names of all the files in the batch
 
 Engineer Story: As an Engineer I want to see unit tests
 1. Implement unit tests
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QHBoxLayout, QVBoxLayout, QComboBox
+from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QHBoxLayout, QVBoxLayout, QComboBox, QFileDialog
 from PyQt6.QtCore import QSize, Qt
 
 
@@ -19,25 +19,81 @@ class MainWindowLayout(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.directory = ""
+
         # Create PyQt elements
-        new_name_preview = QLabel("", self)
+        self.directory_label = QLabel("Directory with files to rename: ", self)
+        self.select_files_to_rename = QPushButton("Select directory with files to rename", self)
+        self.new_name_preview = QLabel("", self)
         new_name_label = QLabel("New name for this file batch", self)
-        new_name_input = QLineEdit(self)
+        self.new_name_input = QLineEdit(self)
+        self.rename_files_btn = QPushButton("Rename files", self)
+        self.rename_files_btn.setEnabled(False)
+
+        # Connect signals
+        self.select_files_to_rename.clicked.connect(self.choose_files)
+        self.new_name_input.textChanged.connect(self.show_preview)
+        self.rename_files_btn.clicked.connect(self.rename_files)
 
         # Add PyQt elements to layout
         layout = QVBoxLayout(self)
-        layout.addWidget(new_name_preview)
+        layout.addWidget(self.directory_label)
+        layout.addWidget(self.select_files_to_rename)
+        layout.addWidget(self.new_name_preview)
 
         horizontal_layout = QHBoxLayout(self)
         horizontal_layout.addWidget(new_name_label)
-        horizontal_layout.addWidget(new_name_input)
+        horizontal_layout.addWidget(self.new_name_input)
 
         layout.addLayout(horizontal_layout)
+        layout.addWidget(self.rename_files_btn)
 
         self.setLayout(layout)
 
+    def choose_files(self):
+        self.directory = os.path.abspath(QFileDialog.getExistingDirectory(self, 'Select directory'))
 
-# Create an instance of QApplication. Pass in sys.argv to allow comman line arguments.
+        self.directory_label.setText("Directory with files to rename: {}".format(self.directory))
+        self.rename_files_btn.setText('Rename files')
+        self.rename_files_btn.setEnabled(True)
+
+    def show_preview(self):
+        input = self.new_name_input.text()
+        str_to_display = "The files will be renamed to: {}_1, {}_2 and so on".format(input, input)
+        self.new_name_preview.setText(str_to_display)
+
+    def rename_files(self):
+        list_of_files = os.listdir(self.directory)
+        print(list_of_files)
+
+        i = 1
+        new_batch_name = self.new_name_input.text()
+
+        for file in list_of_files:
+
+            print('self.directory =', self.directory)
+            print('cwd', os.getcwd())
+
+            old_name, extension = os.path.splitext(os.path.join(self.directory, file))
+            print(old_name, extension)
+            new_name = f'{new_batch_name}_{i}'
+
+            old = os.path.join(self.directory, f'{old_name}{extension}')
+            new = os.path.join(self.directory, f'{new_name}{extension}')
+
+            print(f"Renaming file {old} to {new}")
+
+            os.rename(old, new)
+
+            i += 1
+
+            print(f"Renamed file {file} to {new_name}{extension}")
+
+        self.rename_files_btn.setEnabled(False)
+        self.rename_files_btn.setText('Files renamed succesfully. Choose next directory.')
+
+
+# Create an instance of QApplication. Pass in sys.argv to allow command line arguments.
 app = QApplication(sys.argv)
 
 # Create a Qt widget, which will be our window
